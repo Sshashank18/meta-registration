32c14b623272a1a8c632ec4bf3fa567c3b57c64c
Shashank aggarwal <shashankaggarwal13@gmail.com>
2020-01-23 00:53:21 +0530

    Change

--- environments.js
+++ environments.js
@@ -1,10 +1,10 @@
 module.exports.PORT = 3000;
 module.exports.DOMAIN = `https://metacognition-registration.herokuapp.com/`;
 
-module.exports.DATABASE_HOST = "ec2-3-220-86-239.compute-1.amazonaws.com";
-module.exports.DATABASE_NAME = "d7tpmck9td7uuo";
-module.exports.DATABASE_USER = "whuonxgshkrvol";
+module.exports.DATABASE_HOST = "ec2-54-225-106-93.compute-1.amazonaws.com";
+module.exports.DATABASE_NAME = "d3djk54c13r2e8";
+module.exports.DATABASE_USER = "qzjzbajgzjdezb";
 module.exports.DATABASE_PORT = 5432;
-module.exports.DATABASE_PASSWORD = "ccb4801ca7fc2cba139f6f156d726723996286d4b205e5e4fb94f75fdc3168ea";
+module.exports.DATABASE_PASSWORD = "ac523d698227098cf1734ea4577d245ef62aa01547cb5bebcd76ab9f5589b29e";
 
 
